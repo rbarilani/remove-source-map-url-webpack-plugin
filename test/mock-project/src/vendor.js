0a437d7916ae98952599360c5afb61a9ab9c60da
Ruben Barilani <ruben.barilani.dev@gmail.com>
2020-10-24 23:46:07 +0200

    style: indent tab width to 2 (#10)

--- test/mock-project/src/vendor.js
+++ test/mock-project/src/vendor.js
@@ -1,11 +1,11 @@
 // emulate a minified version of third-party library with sourceMappingURL references
 // # sourceMappingURL=/vendor1.js
 export function vendor1() {
-    console.log("vendor1");
+  console.log("vendor1");
 }
 
 // emulate a minified version of third-party library with sourceMappingURL references
 // # sourceMappingURL=/vendor2.js
 export function vendor2() {
-    console.log("vendor2");
+  console.log("vendor2");
 }
