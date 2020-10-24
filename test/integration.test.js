ba4c29527452afa5b2b6a90507dcdaf4266663fb
Ruben Barilani <ruben.barilani@zalando.de>
2020-10-24 23:43:22 +0200

    style: indent tab width to 2

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,53 +1,48 @@
 const fs = require("fs");
 const webpack = require("webpack");
 const options = require("./mock-project/webpack.config.js");
-const RemoveSourceMapURLWebpackPlugin = require("../index");
 
 describe(`Webpack compiles assets`, () => {
-    it(`without the plugin, '# sourceMappingURL' comments are NOT removed`, async () => {
-        const { stats } = await executeWebpack({
-            ...options,
-            ...{
-                plugins: [],
-            },
-        });
-
-        stats
-            .toJson()
-            .assets.map((x) => x.name)
-            .filter((name) => name.endsWith(".js"))
-            .map((name) =>
-                fs.readFileSync(`${options.output.path}/${name}`, "utf-8")
-            )
-            .forEach((content) => {
-                expect(content.indexOf("sourceMappingURL") !== -1).toBeTruthy();
-            });
+  it(`without the plugin, '# sourceMappingURL' comments are NOT removed`, async () => {
+    const { stats } = await executeWebpack({
+      ...options,
+      ...{
+        plugins: [],
+      },
     });
 
-    it(`with the plugin, '# sourceMappingURL' comments are removed`, async () => {
-        const { stats } = await executeWebpack(options);
-        stats
-            .toJson()
-            .assets.map((x) => x.name)
-            .filter((name) => name.endsWith(".js"))
-            .map((name) =>
-                fs.readFileSync(`${options.output.path}/${name}`, "utf-8")
-            )
-            .forEach((content) => {
-                expect(content.indexOf("sourceMappingURL") === -1).toBeTruthy();
-            });
-    });
+    stats
+      .toJson()
+      .assets.map((x) => x.name)
+      .filter((name) => name.endsWith(".js"))
+      .map((name) => fs.readFileSync(`${options.output.path}/${name}`, "utf-8"))
+      .forEach((content) => {
+        expect(content.indexOf("sourceMappingURL") !== -1).toBeTruthy();
+      });
+  });
+
+  it(`with the plugin, '# sourceMappingURL' comments are removed`, async () => {
+    const { stats } = await executeWebpack(options);
+    stats
+      .toJson()
+      .assets.map((x) => x.name)
+      .filter((name) => name.endsWith(".js"))
+      .map((name) => fs.readFileSync(`${options.output.path}/${name}`, "utf-8"))
+      .forEach((content) => {
+        expect(content.indexOf("sourceMappingURL") === -1).toBeTruthy();
+      });
+  });
 });
 
 function executeWebpack(options) {
-    return new Promise((resolve, reject) => {
-        webpack(options, (err, stats) => {
-            if (err) {
-                return reject(err);
-            } else if (stats.hasErrors()) {
-                return reject(new Error(stats.toString()));
-            }
-            return resolve({ stats });
-        });
+  return new Promise((resolve, reject) => {
+    webpack(options, (err, stats) => {
+      if (err) {
+        return reject(err);
+      } else if (stats.hasErrors()) {
+        return reject(new Error(stats.toString()));
+      }
+      return resolve({ stats });
     });
+  });
 }
